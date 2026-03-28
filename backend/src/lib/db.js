34e37dc470149f4fd9e53416084487948fd1a99b
abhay pundora <abhaypundora17@gmail.com>
2026-03-28 18:34:19 +0530

    validation check added for DB_URL

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -3,6 +3,9 @@ import "dotenv/config";
 
 export const connectDB = async () => {
   try {
+    if (!process.env.DB_URL) {
+      throw new Error("DB_URL is not defined in environment variables");
+    }
     const conn = await mongoose.connect(process.env.DB_URL);
     console.log("✅ Connected to MongoDB:", conn.connection.host);
   } catch (error) {
