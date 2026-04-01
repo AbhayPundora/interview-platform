ea665cdd05b9eac27ca0fee2a49f68dcf68d21c0
abhay pundora <abhaypundora17@gmail.com>
2026-04-01 17:19:52 +0530

    adding response msg consistency

--- backend/src/middleware/protectRoute.js
+++ backend/src/middleware/protectRoute.js
@@ -7,12 +7,14 @@ export const protectRoute = [
     try {
       const clerkId = req.auth().userId; // coming from clerk
       if (!clerkId)
-        return res.status(401).json({ msg: "Unauthorized - invalid token" });
+        return res
+          .status(401)
+          .json({ message: "Unauthorized - invalid token" });
 
       // find user in db by clerk ID
       const user = await User.findOne({ clerkId });
 
-      if (!user) return res.status(404).json({ msg: "User not found" });
+      if (!user) return res.status(404).json({ message: "User not found" });
 
       // attach user to req
       req.user = user;
@@ -20,7 +22,7 @@ export const protectRoute = [
       next();
     } catch (error) {
       console.log("Error in protectRoute middleware", error);
-      res.status(500).json({ msg: "Internal Server Error" });
+      res.status(500).json({ message: "Internal Server Error" });
     }
   },
 ];
